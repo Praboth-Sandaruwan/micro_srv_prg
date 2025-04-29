e96f6d78b4685b05c6c6737f1fb0982614d7a550
Praboth Sandaruwan <bleezbub12@gmail.com>
2025-04-29 19:26:09 +0530

    cmm

--- frontend/praboth-frontend/src/api/axiosInstance.js
+++ frontend/praboth-frontend/src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8006/deliverydriver/api/v1', 
+  baseURL: 'http://localhost:8016/deliverydriver/api/v1', 
 });
 
 
--- frontend/praboth-frontend/src/api/notificationapi.js
+++ frontend/praboth-frontend/src/api/notificationapi.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8009/notification/ctrl/api/v1',
+  baseURL: 'http://localhost:8015/notification/ctrl/api/v1',
 });
 
 
